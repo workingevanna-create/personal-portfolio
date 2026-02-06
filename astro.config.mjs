88b18e747c84e35c2ba2a065d19fa2694d6fa9c5
施翌雯 <shiyiwen@Evannas-MAC.local>
2026-02-07 00:01:43 +0800

    chore: disable sitemap integration for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx(), icon()],
+  integrations: [tailwind(), mdx(), icon()],
   site: 'https://workingevanna-create.github.io',
   base: '/personal-portfolio/',
 });
